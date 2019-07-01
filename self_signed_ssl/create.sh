159203e68f823eb297b9cd3cdf112c50e597bbe8
Siim Aus <herrbpl@gmail.com>
2019-07-01 09:29:03 +0000

    improved selfsigned cert creation

--- self_signed_ssl/create.sh
+++ self_signed_ssl/create.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-openssl genrsa -out test.key 1024
-openssl req -new -key test.key -out test.csr
-openssl x509 -req -days 365 -in test.csr -signkey test.key -out test.crt
-openssl pkcs12 -export -out test.pfx -inkey test.key -in test.crt
+
+
+if [ -z "$1" ]; then
+	echo "Usage: creat.sh <FQDN>"
+	echo "Must cpecify FQDN as a parameter"
+	exit 1;
+fi
+
+FQDN=`basename $1`
+
+if [ -z "$FQDN" ]; then
+	echo "Must cpecify FQDN as a parameter"
+	exit 1
+fi
+
+if [ -f "$FQDN.key" ]; then
+	echo "$FQDN.key already exists. Remove or rename it beore trying again"
+	exit 1
+fi
+
+
+echo "Creating ${FQDN} self-signed key";
+
+openssl genrsa -out ${FQDN}.key 1024
+openssl req -new -key ${FQDN}.key -out ${FQDN}.csr
+openssl x509 -req -days 365 -in ${FQDN}.csr -signkey ${FQDN}.key -out ${FQDN}.crt
+openssl pkcs12 -export -out ${FQDN}.pfx -inkey ${FQDN}.key -in ${FQDN}.crt
+
