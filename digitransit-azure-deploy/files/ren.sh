c9300babafe799dfaafe307df65fc000344fb669
jz <jz@centos76.localdomain>
2019-10-03 16:14:47 +0300

    rename script testing -> prod

---
+++ digitransit-azure-deploy/files/ren.sh
@@ -0,0 +1,50 @@
+cp digitransit-docker-gc-testing.json               digitransit-docker-gc-prod.json              
+cp digitransit-performance-tests-testing.json       digitransit-performance-tests-prod.json      
+cp digitransit-proxy-testing.json                   digitransit-proxy-prod.json                  
+cp digitransit-site-testing.json                    digitransit-site-prod.json                   
+cp digitransit-ui-matka-testing.json                digitransit-ui-matka-prod.json               
+cp graphiql-testing.json                            graphiql-prod.json                           
+cp hslalert-testing.json                            hslalert-prod.json                           
+cp hsl-map-server-testing.json                      hsl-map-server-prod.json                     
+cp mosquitto-server-testing.json                    mosquitto-server-prod.json                   
+cp navigator-server-testing.json                    navigator-server-prod.json                   
+cp opentripplanner-data-con-estonia-testing.json    opentripplanner-data-con-estonia-prod.json   
+cp opentripplanner-estonia-testing.json             opentripplanner-estonia-prod.json            
+cp osmtracker-testing.json                          osmtracker-prod.json                         
+cp otp-data-builder-dev-testing.json                otp-data-builder-dev-prod.json               
+cp otp-data-builder-prod-testing.json               otp-data-builder-prod-prod.json              
+cp otp-data-builder-test-testing.json               otp-data-builder-test-prod.json              
+cp pelias-api-testing.json                          pelias-api-prod.json                         
+cp pelias-data-container-testing.json               pelias-data-container-prod.json              
+cp pelias-interpolation-testing.json                pelias-interpolation-prod.json               
+cp pelias-libpostal-testing.json                    pelias-libpostal-prod.json                   
+cp pelias-pip-testing.json                          pelias-pip-prod.json                         
+cp pelias-placeholder-testing.json                  pelias-placeholder-prod.json                 
+cp raildigitraffic2gtfsrt-testing.json              raildigitraffic2gtfsrt-prod.json             
+cp rt-estonia-vehicles-service-testing.json         rt-estonia-vehicles-service-prod.json        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
